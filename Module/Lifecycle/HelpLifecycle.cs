64a2097e78f4516b3312ddb09edbe2aa4795cc54
Corentin Z <corentinz@outlook.fr>
2023-11-18 22:30:30 +0100

    [Lifecycle] 📚 Help lifecycle

--- Module/Lifecycle/HelpLifecycle.cs
+++ Module/Lifecycle/HelpLifecycle.cs
@@ -4,17 +4,35 @@
     {
         public void CheckArgs(string[] args)
         {
-            throw new NotImplementedException();
+            if (args?.Length > 0) throw new Exception("Version lifecycle can't have arguments");
         }
 
-        public void Execute()
-        {
-            throw new NotImplementedException();
-        }
+        public void Execute() { }
 
         public void Init(string[] args)
         {
-            throw new NotImplementedException();
+            Console.Write(
+@"CHECK : -C {-c|-f} [-v]
+	-C : Vérifie les configurations
+	-c : structure de la configuration
+	-f : l'appel, la réception et le parsing des flux configurés
+	-v : mode verbeux
+
+VERSION : -V
+	-V : Affiche la version
+
+HELP : -H
+	-H : Affiche l'aide
+
+LOGS : -L [xxx]
+	-L : Affiche les logs
+	[xxx] : Les xxx dernières lignes
+
+TESTS : -T {-u|-i} [-v]
+	-T : Exécute les tests
+	-u : unitaires
+	-i : d'intégration
+	-v : mode verbeux");
         }
     }
 }
