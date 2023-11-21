c23a422b5d17aeb84410aec2ed380d8b4f50812a
Corentin Z <corentinz@outlook.com>
2023-11-21 01:05:53 +0100

    [Version] ♻️ Upgrade version -> 0.2.0

--- sk-news-batch/Service/VersionProvider.cs
+++ sk-news-batch/Service/VersionProvider.cs
@@ -8,7 +8,7 @@
 
         private VersionProvider()
         {
-            Version = new(0, 1, 0);
+            Version = new(0, 2, 0);
         }
 
         private static VersionProvider instance;
